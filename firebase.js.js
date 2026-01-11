e64fe47d89adf553bde4a52e014d37a4e78af1a6
akaiwang523 <akaiwang523@gmail.com>
2026-01-12 01:32:34 +0800

    Update firebase.js.js

--- firebase.js.js
+++ firebase.js.js
@@ -1,20 +1,17 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// ▼▼▼ 把這裡換成你剛剛在 Firebase 複製的那一串 ▼▼▼
+// ▼▼▼ 這裡請務必換成您剛剛在 Firebase 後台複製的那一串 Config ▼▼▼
+// 如果您忘記複製，請去 Firebase Console -> 專案設定 (齒輪圖示) -> 一般 -> 下滑到底部就看得到
 const firebaseConfig = {
-  apiKey: "AIzaSyDxxxx...",
-  authDomain: "xxxx.firebaseapp.com",
-  projectId: "xxxx",
-  storageBucket: "xxxx.appspot.com",
-  messagingSenderId: "xxxx",
-  appId: "xxxx"
+  apiKey: "AIzaSyCPM6djPYyZ_zMyf8fJ2qRnayHnLXIj-NE",
+  authDomain: "machine-45f27.firebaseapp.com",
+  projectId: "machine-45f27",
+  storageBucket: "machine-45f27.firebasestorage.app",
+  messagingSenderId: "576671716540",
+  appId: "1:576671716540:web:429d8ad780dfddfd2c2cb9",
+  measurementId: "G-2874L084WT"
 };
-// ▲▲▲▲▲▲
 
-// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
-
-// 匯出資料庫功能讓 App.jsx 使用
 export const db = getFirestore(app);
