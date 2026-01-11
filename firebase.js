cc738ea28964ae7b4185bf55a95faed7e73e762c
akaiwang523 <akaiwang523@gmail.com>
2026-01-12 01:46:05 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // 確保這行有引入
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCPM6djPYyZ_zMyf8fJ2qRnayHnLXIj-NE",
   authDomain: "machine-45f27.firebaseapp.com",
@@ -15,7 +10,7 @@ const firebaseConfig = {
   appId: "1:576671716540:web:429d8ad780dfddfd2c2cb9",
   measurementId: "G-2874L084WT"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// ▼▼▼ 關鍵在這裡！一定要有 export ▼▼▼
+export const db = getFirestore(app);
